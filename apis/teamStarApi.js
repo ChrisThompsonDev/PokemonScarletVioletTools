95c81d9548ce7ebe2b5b4f688b80fe8b39d79f08
ChrisThompsonDev <ChrisThompsonDev@protonmail.com>
2022-12-02 09:45:50 -0500

    changed Team Star and Titan images

--- apis/teamStarApi.js
+++ apis/teamStarApi.js
@@ -1,31 +1,31 @@
 const teamStar = [
     {
         id: 'teamStarGiacomo', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/0/0f/VSGiacomo.png/144px-VSGiacomo.png",
+        img: "images/giacomo.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/1/1a/SVbadge_StarfallStreet_Dark.png/40px-SVbadge_StarfallStreet_Dark.png",
         title: "Giacomo",
     },
     {
         id: 'teamStarMela', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/c/c5/VSMela_S.png/116px-VSMela_S.png",
+        img: "images/mela.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/4/46/SVbadge_StarfallStreet_Fire.png/40px-SVbadge_StarfallStreet_Fire.png",
         title: "Mela",
     },
     {
         id: 'teamStarAtticus', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/2/2e/VSAtticus.png/102px-VSAtticus.png",
+        img: "images/atticus.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/4/4b/SVbadge_StarfallStreet_Poison.png/40px-SVbadge_StarfallStreet_Poison.png",
         title: "Atticus",
     },
     {
         id: 'teamStarOrtega', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/c/ca/VSOrtega.png/127px-VSOrtega.png",
+        img: "images/ortega.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/f/f0/SVbadge_StarfallStreet_Fairy.png/40px-SVbadge_StarfallStreet_Fairy.png",
         title: "Ortega",
     },
     {
         id: 'teamStarEri', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/5/53/VSEri.png/143px-VSEri.png",
+        img: "images/eri.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/0/03/SVbadge_StarfallStreet_Fighting.png/40px-SVbadge_StarfallStreet_Fighting.png",
         title: "Eri",
     },
--- apis/titansApi.js
+++ apis/titansApi.js
@@ -1,33 +1,33 @@
 const titans = [
     {
         id: 'titanKlawf', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/d/d8/962Klawf.png/150px-962Klawf.png",
+        img: "images/stonyCliffTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/3/3d/SVbadge_PathOfLegends_Rock.png/40px-SVbadge_PathOfLegends_Rock.png',
-        title: "Klawf",
+        title: "Stony Cliff Titan",
     },
     {
         id: 'titanBombirdier', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/5/52/Bombirdier.png/150px-Bombirdier.png",
+        img: "images/openSkyTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/3/37/SVbadge_PathOfLegends_Flying.png/40px-SVbadge_PathOfLegends_Flying.png',
-        title: "Bombirdier",
+        title: "Open Sky Titan",
     },
     {
         id: 'titanOrthworm', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/f/f1/Orthworm.png/150px-Orthworm.png",
+        img: "images/lurkingSteelTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/1/11/SVbadge_PathOfLegends_Steel.png/40px-SVbadge_PathOfLegends_Steel.png',
-        title: "Orthworm",
+        title: "Lurking Steel Titan",
     },
     {
         id: 'titanTuskTreads', 
-        img: "images/tuskTreads.png",
+        img: "images/quakingEarthTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/1/11/SVbadge_PathOfLegends_Ground.png/40px-SVbadge_PathOfLegends_Ground.png',
-        title: "Great Tusk / Iron Treads",
+        title: "Quaking Earth Titan",
     },
     {
         id: 'titanDodonzo', 
-        img: "https://archives.bulbagarden.net/media/upload/thumb/d/d9/Menu_SV_Dondozo.png/150px-Menu_SV_Dondozo.png",
+        img: "images/falseDragon.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/d/dc/SVbadge_PathOfLegends_Dragon.png/40px-SVbadge_PathOfLegends_Dragon.png',
-        title: "Dodonzo",
+        title: "False Dragon",
     },
     
 ]
