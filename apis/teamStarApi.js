07cc5904149d23a932e957ae300c439308c7c88c
ChrisThompsonDev <ChrisThompsonDev@protonmail.com>
2022-12-12 09:46:26 -0500

    updated story page and added to tools page

--- apis/teamStarApi.js
+++ apis/teamStarApi.js
@@ -3,31 +3,31 @@ const teamStar = [
         id: 'teamStarGiacomo', 
         img: "images/giacomo.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/1/1a/SVbadge_StarfallStreet_Dark.png/40px-SVbadge_StarfallStreet_Dark.png",
-        title: "Giacomo",
+        title: "05 - Giacomo",
     },
     {
         id: 'teamStarMela', 
         img: "images/mela.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/4/46/SVbadge_StarfallStreet_Fire.png/40px-SVbadge_StarfallStreet_Fire.png",
-        title: "Mela",
+        title: "07 - Mela",
     },
     {
         id: 'teamStarAtticus', 
         img: "images/atticus.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/4/4b/SVbadge_StarfallStreet_Poison.png/40px-SVbadge_StarfallStreet_Poison.png",
-        title: "Atticus",
+        title: "10 - Atticus",
     },
     {
         id: 'teamStarOrtega', 
         img: "images/ortega.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/f/f0/SVbadge_StarfallStreet_Fairy.png/40px-SVbadge_StarfallStreet_Fairy.png",
-        title: "Ortega",
+        title: "16 - Ortega",
     },
     {
         id: 'teamStarEri', 
         img: "images/eri.png",
         badge: "https://archives.bulbagarden.net/media/upload/thumb/0/03/SVbadge_StarfallStreet_Fighting.png/40px-SVbadge_StarfallStreet_Fighting.png",
-        title: "Eri",
+        title: "18 - Eri",
     },
     
 ]
--- apis/titansApi.js
+++ apis/titansApi.js
@@ -3,31 +3,31 @@ const titans = [
         id: 'titanKlawf', 
         img: "images/stonyCliffTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/3/3d/SVbadge_PathOfLegends_Rock.png/40px-SVbadge_PathOfLegends_Rock.png',
-        title: "Stony Cliff Titan",
+        title: "02 - Stony Cliff Titan",
     },
     {
         id: 'titanBombirdier', 
         img: "images/openSkyTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/3/37/SVbadge_PathOfLegends_Flying.png/40px-SVbadge_PathOfLegends_Flying.png',
-        title: "Open Sky Titan",
+        title: "04 - Open Sky Titan",
     },
     {
         id: 'titanOrthworm', 
         img: "images/lurkingSteelTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/1/11/SVbadge_PathOfLegends_Steel.png/40px-SVbadge_PathOfLegends_Steel.png',
-        title: "Lurking Steel Titan",
+        title: "08 - Lurking Steel Titan",
     },
     {
         id: 'titanTuskTreads', 
         img: "images/quakingEarthTitan.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/1/11/SVbadge_PathOfLegends_Ground.png/40px-SVbadge_PathOfLegends_Ground.png',
-        title: "Quaking Earth Titan",
+        title: "13 - Quaking Earth Titan",
     },
     {
         id: 'titanDodonzo', 
         img: "images/falseDragon.png",
         badge: 'https://archives.bulbagarden.net/media/upload/thumb/d/dc/SVbadge_PathOfLegends_Dragon.png/40px-SVbadge_PathOfLegends_Dragon.png',
-        title: "False Dragon",
+        title: "17 - False Dragon",
     },
     
 ]
